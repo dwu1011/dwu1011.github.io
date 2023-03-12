784a2c5d704b4f708c3cbf88309a13d155452ebf
dwu1011 <daviddwu22@gmail.com>
2023-03-12 14:18:38 -0400

    changed to index.html

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,28 @@
 console.log("HELLO");
 var head = document.getElementsByClassName("navbar")[0];
+var links = head.getElementsByTagName("a");
 window.onscroll = function(event){
-    var scroll = window.pageYOffset;
-    if(scroll < 100){
-        head.style.backgroundColor = 'transparent';
-    }else{
-        head.style.backgroundColor = 'black';
+    for(var i of links){
+        i.style.color = "white";
     }
 }
 
+const onScrollStop = callback => {
+    let isScrolling;
+    window.addEventListener('scroll', e => {
+        head.style.backgroundColor = 'black';
+        clearTimeout(isScrolling);
+        isScrolling = setTimeout(() => {
+            callback();
+        }, 150);
+      },
+      false
+    );
+};
+
+onScrollStop(() => {
+    head.style.backgroundColor = 'transparent';
+    for(var i of links){
+        i.style.color = "black";
+    }
+});
